1c715e1cbd4e6ad13e1c245a72b43cb3e9e70c0a
Yusuke Ando <yandod@gmail.com>
2016-01-03 19:25:28 +0900

    add json

---
+++ convert.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+require 'csv'
+require 'json'
+
+data = []
+csv = CSV.read('./quiz.csv',:headers=>true)
+csv.each do |row|
+  data.push({
+    "question" => row["Text"],
+    "choice" => [row["a1"], row["a2"], row["a3"], row["a4"],],
+    "answer" => row["a1"]
+  })
+end
+
+puts data.to_json
